e2b9093ef9bf7fffa414ad99ec91ce55e9d9213f
Andri <a.halili2004@gmail.com>
2025-01-05 18:11:15 +0100

    Add Spring Boot project to backend folder

---
+++ calories-tracker-backend/calorie-tracker/src/main/java/com/grupi2/calorie_tracker/CalorieTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.grupi2.calorie_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieTrackerApplication.class, args);
+	}
+
+}
