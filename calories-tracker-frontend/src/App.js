54568e4211f49ba2ee2724861a90fa5b8bc11215
Andri <a.halili2004@gmail.com>
2025-01-05 18:40:01 +0100

    Set up React frontend and start development server

---
+++ calories-tracker-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'; // <-- Add this line
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Calories Tracker</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
